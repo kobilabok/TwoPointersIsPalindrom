79fcc781a74c3452ca8629eae89d9383add39080
Kobi Labok <kobilabok@gmail.com>
2024-06-09 08:59:48 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+var input = "aurora"; //racecar, aurora
+
+Console.WriteLine(IsPalindrom(input));
+
+bool IsPalindrom(string s){
+
+    int left = 0;
+    int right = s.Length - 1;
+
+     while(left < right){
+
+        if(s[left] != s[right]){
+            return false;
+        }
+        left++;
+        right--;
+     }
+
+    return true;
+}
