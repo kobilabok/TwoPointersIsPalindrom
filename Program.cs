499271d673e5bd5b2cc2cab82db6c41cd71b0f40
Kobi Labok <kobilabok@gmail.com>
2024-06-09 10:05:17 -0600

    CheckForTarget using Two Pointer method was added

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 //Console.WriteLine("Hello, World!");
 
 
+// Is Palindrom
+
 var input = "aurora"; //racecar, aurora
 
 Console.WriteLine(IsPalindrom(input));
@@ -21,4 +23,32 @@ bool IsPalindrom(string s){
      }
 
     return true;
+}
+
+
+// Two Numbers - Return True if sum of two numbers equal target number
+int[] numbers = [1, 2, 4, 6, 8, 9, 14, 15];
+int targetNum = 13;
+
+Console.WriteLine(CheckForTarget(numbers, targetNum));
+
+bool CheckForTarget(int[] nums, int target){
+    int left = 0;
+    int right = nums.Length - 1;
+
+    while(left < right){
+
+        // current number is current sum
+        int curr = nums[left] + nums[right];
+        if(curr == target){
+            return true;
+        }
+        if(curr > target){
+            right--;
+        }
+        else{
+            left++;
+        }
+    }
+    return false;
 }
--- obj/Debug/net8.0/TwoPointersIsPalindrom.AssemblyInfo.cs
+++ obj/Debug/net8.0/TwoPointersIsPalindrom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwoPointersIsPalindrom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79fcc781a74c3452ca8629eae89d9383add39080")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwoPointersIsPalindrom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwoPointersIsPalindrom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
